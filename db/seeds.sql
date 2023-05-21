292badb45880b938e368a151a6a76c922adbe0c5
Juan Martinez <juanmartinez@Juans-MacBook.local>
2023-05-20 22:08:20 -0400

    added case/switch statements that are needed

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO department (id, name)
+VALUES
+(1, 'Engineering'),
+(2, 'Marketing'),
+(3, 'Finance'),
+(4, 'Human Resources'),
+(5, 'Sales'),
+(6, 'Management'),
+(7, 'Information Technology')
+    
--- index.js
+++ index.js
@@ -19,7 +19,48 @@ inquirer.prompt(
     }   
 ).then(result=>{
     console.log(result.choice);
+    const selectedChoice = result.choice
+    switch(selectedChoice){
+        case'view all departments':
+            viewAllDepartments()
+            break;
+        case 'view all roles':
+            viewAllRoles()
+            break;
+        case 'view all employees':
+            viewAllEmployees()
+            break;
+        case 'add a department':
+            addDepartment()
+            break;
+        case 'add an employee':
+            addEmployee()
+            break
+        case 'add a role':
+            addRole()
+            break;
+        case 'update employee role':
+            updateRole()
+            break;
+    }
 })
 
 }
+function viewAllDepartments (){
+db.query('SELECT * from department',(err, result)=>{
+    if (err){
+        console.log(err);
+    }else {
+        console.table(result)
+        init()
+    }
+})
+}
+function viewAllRoles (){
+
+}
+function viewAllEmployees(){
+
+}
+
 init()
